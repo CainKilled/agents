a006b768ad244ac0238ec23007ce7db5ab9b2541
Adam Nagle <162903473+CainKilled@users.noreply.github.com>
2025-06-10 02:12:30 -0600

    Update livekit/__init__.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- livekit/__init__.py
+++ livekit/__init__.py
@@ -5,7 +5,9 @@ __path__ = extend_path(__path__, __name__)
 
 _here = Path(__file__).resolve().parent
 # include livekit-agents package
-__path__.append(str(_here.parent / "livekit-agents" / "livekit"))
+_agents_path = _here.parent / "livekit-agents" / "livekit"
+if _agents_path.is_dir():
+    __path__.append(str(_agents_path))
 # include all plugin packages
 for p in (_here.parent / "livekit-plugins").glob("livekit-plugins-*/livekit"):
     __path__.append(str(p))
